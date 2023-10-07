2120aaf0156a5fa47bd512ffedc54c6b7cb6c8d6
Haly Yampolska <halyna.yampolska@gmail.com>
2023-10-07 21:29:44 +0300

    Started create plugin based on React

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '6eaff5baccffe478639c');
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+/*
+Plugin Name: Are You Paying Attention Quiz
+    Description: Give youe readers a multiple choice question.
+    Version: 1.0
+    Author: Halyna 
+    Author URL: https://halynayampolska.com/
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly 
+
+class AreYouPayingAttention {
+    function __construct() {
+        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    }
+
+    function adminAssets() {
+        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+    }
+}
+
+$AreYouPayingAttention = new AreYouPayingAttention();
